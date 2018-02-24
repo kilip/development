d610f153527de11be6e21c56b8686a254a48fdf4
Anthonius Munthi <me@itstoni.com>
2018-02-24 15:03:55 +0800

    added README file ref #1, #2, #3

--- etc/docker/docker-entrypoint.sh
+++ etc/docker/docker-entrypoint.sh
@@ -7,11 +7,11 @@ if [ "${1#-}" != "$1" ]; then
 fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
-	#if [ "$APP_ENV" != 'prod' ]; then
+	if [ "$APP_ENV" != 'prod' ]; then
 		# composer install --prefer-dist --no-progress --no-suggest --no-interaction
-		# bin/console assets:install
-		# bin/console doctrine:schema:update -f
-	#fi
+		bin/console assets:install
+		bin/console doctrine:schema:update -f
+	fi
 
 	# Permissions hack because setfacl does not work on Mac and Windows
 	chown -R www-data var
