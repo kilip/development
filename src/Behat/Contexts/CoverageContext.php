819537ab2f0076367b9e371b32b4a1c0d27d293f
Anthonius Munthi <me@itstoni.com>
2018-03-12 17:37:48 +0800

    debug travis integration ref #2

---
+++ src/Behat/Contexts/CoverageContext.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Paroki project.
+ *
+ * (c) Anthonius Munthi <me@itstoni.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Paroki\Behat\Contexts;
+
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use SebastianBergmann\CodeCoverage\CodeCoverage;
+use SebastianBergmann\CodeCoverage\Filter;
+use SebastianBergmann\CodeCoverage\Report\PHP;
+
+class CoverageContext implements Context
+{
+    /**
+     * @var CodeCoverage
+     */
+    private static $coverage;
+
+    /**
+     * @BeforeSuite
+     */
+    public static function setup()
+    {
+        $filter = new Filter();
+        $filter->addDirectoryToWhitelist(getcwd().'/src/');
+        $filter->removeDirectoryFromWhitelist(getcwd().'/src/Behat');
+        $filter->removeDirectoryFromWhitelist(getcwd().'/src/*/Test');
+        $filter->removeDirectoryFromWhitelist(getcwd().'/src/*/Tests');
+        $filter->removeDirectoryFromWhitelist(getcwd().'/src/*/Spec');
+        $filter->removeDirectoryFromWhitelist(getcwd().'/src/*/Resources');
+        self::$coverage = new CodeCoverage(null, $filter);
+    }
+
+    /**
+     * @AfterSuite
+     */
+    public static function tearDown()
+    {
+        $feature = getenv('FEATURE') ?: 'behat';
+        (new PHP())->process(self::$coverage, getcwd()."/build/coverage/php/coverage-$feature.cov");
+    }
+
+    /**
+     * @BeforeScenario
+     */
+    public function startCoverage(BeforeScenarioScope $scope)
+    {
+        self::$coverage->start("{$scope->getFeature()->getTitle()}::{$scope->getScenario()->getTitle()}");
+    }
+
+    /**
+     * @AfterScenario
+     */
+    public function stopCoverage()
+    {
+        self::$coverage->stop();
+    }
+}
