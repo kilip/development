2d8598eb9fdc1b81c57916b39b742624f1d82a99
Anthonius Munthi <me@itstoni.com>
2018-03-20 23:01:01 +0800

    travis debug

--- etc/travis/lib/common.sh
+++ etc/travis/lib/common.sh
@@ -12,3 +12,23 @@ run_command() {
 print_header() {
     echo -e "$(bold "$1"): $(bold_green "$2")"
 }
+
+# Argument 1: Text
+bold() {
+    echo -e "\e[1m$1\e[0;20m"
+}
+
+# Argument 1: Text
+bold_green() {
+    echo -e "\e[33;1m$1\e[0;20m"
+}
+
+# Argument 1: Text
+red() {
+    echo -e "\e[31m$1\e[0m"
+}
+
+# Argument 1: Text
+bold_red() {
+    echo -e "\e[31;1m$1\e[0;20m"
+}
