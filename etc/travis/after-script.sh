57476e2733d951d3dacdad6e71576eee48016ca7
Anthonius Munthi <me@itstoni.com>
2018-03-21 01:40:24 +0800

    fixed after_script command

--- etc/travis/after-script.sh
+++ etc/travis/after-script.sh
@@ -8,14 +8,14 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/lib/common.sh"
 
 if [[ $SIAP_SUITE == "coverage" ]]; then
     run_command "wget https://phar.phpunit.de/phpcov.phar"
-    run_command "chmod +x phpcov.phar"
-    run_command "./phpcov.phar merge build/coverage --clover build/logs/backend/clover.xml"
+    chmod +x phpcov.phar
+    ./phpcov.phar merge build/coverage --clover build/logs/backend/clover.xml
 
     print_header "uploading code coverage"
-    run_command "wget https://codecov.io/bash > codecov.sh"
-    run_command "chmod +x codecov.sh"
-    run_command "./codecov.sh -F backend -s build/logs/backend -f \"*.xml\""
-    run_command "./codecov.sh -F frontend -s build/logs/frontend -f \"*.xml\""
+    wget https://codecov.io/bash > codecov.sh
+    chmod +x codecov.sh
+    ./codecov.sh -F backend -s build/logs/backend -f "*.xml"
+    ./codecov.sh -F frontend -s build/logs/frontend -f "*.xml"
 fi
 
 exit ${code}
