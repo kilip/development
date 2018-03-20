56387e62f6be41416e8dc92d1373c3ae0581b38c
Anthonius Munthi <me@itstoni.com>
2018-03-21 00:14:15 +0800

    fix scripts

--- etc/travis/before-install.sh
+++ etc/travis/before-install.sh
@@ -11,18 +11,18 @@ run_command "echo \"memory_limit=-1\" >> ~/.phpenv/versions/$(phpenv version-nam
 
 run_command "export PATH=\"$PATH:$HOME/.composer/vendor/bin\""
 
-if [ $SIAP_SUITE != "coverage" ]; then
+if [[ $SIAP_SUITE != "coverage" ]]; then
     print_header "disable xdebug"
     run_command "phpenv config-rm xdebug.ini || echo \"xdebug not available\""
 fi
 
-if [ ${TRAVIS_PHP_VERSION:0:3} == "7.1" ]; then
+if [[ ${TRAVIS_PHP_VERSION:0:3} == "7.1" ]]; then
     run_command "echo yes | pecl install apcu"
 else
     run_command "echo \"extension = apcu.so\" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini"
 fi
 
-if [ $SIAP_SUITE == "deploy" ] || [ $SIAP_SUITE == "coverage" ] || [ $SIAP_SUITE == "frontend" ]; then
+if [[ $SIAP_SUITE == "deploy" ]] || [[ $SIAP_SUITE == "coverage" ]] || [[ $SIAP_SUITE == "frontend" ]]; then
     sudo apt-get -y install software-properties-common python-software-properties
     sudo -E apt-add-repository -y ppa:ubuntu-toolchain-r/test
     sudo apt update -y
